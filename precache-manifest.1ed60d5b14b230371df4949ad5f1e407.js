2bb545868c80a314837faedd1f61411be7633ad9
hustcc <i@hust.cc>
2020-11-21 15:04:59 +0800

    Add files via upload

---
+++ precache-manifest.1ed60d5b14b230371df4949ad5f1e407.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23220b0e4136fd10fc2df63647394d8c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "68635f60ccb8fd671ebc",
+    "url": "./static/css/2.405d60bc.chunk.css"
+  },
+  {
+    "revision": "c25cbccf152b30239600",
+    "url": "./static/css/main.37375457.chunk.css"
+  },
+  {
+    "revision": "68635f60ccb8fd671ebc",
+    "url": "./static/js/2.11045267.chunk.js"
+  },
+  {
+    "revision": "8ffd1727d77a80c9f45f66f202a2911d",
+    "url": "./static/js/2.11045267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25cbccf152b30239600",
+    "url": "./static/js/main.2c02a4fc.chunk.js"
+  },
+  {
+    "revision": "29c7a6547537100ade2e",
+    "url": "./static/js/runtime-main.24e87457.js"
+  },
+  {
+    "revision": "cbc4de9a2d6806671f3e332861221965",
+    "url": "./static/media/antv.cbc4de9a.svg"
+  }
+]);
